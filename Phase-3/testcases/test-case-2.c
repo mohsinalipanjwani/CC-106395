698c391b40395fb3374f62435a49d12eeeafe86a
mohsinalipanjwani <47965706+mohsinalipanjwani@users.noreply.github.com>
2021-04-24 17:35:33 +0500

    Create test-case-2.c

---
+++ Phase-3/testcases/test-case-2.c
@@ -0,0 +1,28 @@
+/*
+Test Case 2
+ - Left hand side of an assignment statement should be an lvalue
+The output in lex/yacc should identify those assignment statements whose left habd side is not an lvalue and stray characters must be detected as an error
+*/
+
+
+int main()
+{
+
+  `
+  @ -
+  total = x @ y;
+
+	int a = 4;
+
+	if(a > 0 )
+	{
+		printf("a is positive");
+		a * 2 = a;
+	}
+	else
+	{
+		printf("a is negative");
+		a = a * 2;
+	}
+
+}
